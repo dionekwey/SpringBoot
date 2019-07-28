d64cfd41d6dd1579e4cdbedea6b2e2d0ffe6acbb
dionekwey <dione_cezar@hotmail.com>
2019-07-28 17:58:46 -0300

    Add files via upload

---
+++ src/main/java/br/com/app/repository/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package br.com.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.com.app.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+insert into Usuario (id, nome) values (1, 'Usuário 1');
+insert into Usuario (id, nome) values (2, 'Usuário 2');
+insert into Usuario (id, nome) values (3, 'Usuário 3');
+insert into Usuario (id, nome) values (4, 'Usuário 4');
+insert into Usuario (id, nome) values (4, 'Usuário 5');
